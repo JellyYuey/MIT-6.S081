66a33c77ebc88661a6b5b53df6f80eaaedc0424a
Jelly <lyy08071106@gmail.com>
2024-07-25 23:14:42 -0700

    lab2 is changed

--- kernel/proc.c
+++ kernel/proc.c
@@ -304,6 +304,7 @@ fork(void)
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
+  
 
   release(&np->lock);
 
@@ -313,6 +314,8 @@ fork(void)
 
   acquire(&np->lock);
   np->state = RUNNABLE;
+  // 子进程复制父进程的 mask 
+  np->mask = p->mask;
   release(&np->lock);
 
   return pid;
@@ -653,4 +656,28 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
 }
